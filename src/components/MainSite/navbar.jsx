72994aa199dccaeb2c95bebe2b95372aabd55380
RAHUL-PARLANi <rahulparlani112@gmail.com>
2022-11-03 10:13:06 +0530

    hh

--- src/components/MainSite/navbar.jsx
+++ src/components/MainSite/navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     <div className='NavBar-main'>
        <div className='news'> WELCOME TO BHOOMI EandD TRADING COMPANY'S OFFICIAL WEBSITE!</div>
        <div className='Navbar-logo'>
-        <div style={{'display':'flex','alignItems':'center'}}><div id='relo' onClick={()=>{open()}} style={{'border':'3px solid #ffbd59','fontSize':'32px','width':'40px','height':'45px','color':"#ffbd59",'marginLeft':'5px','textAlign':'center','borderRadius':'5px'}}><FontAwesomeIcon icon="fa-solid fa-bars" /></div><Link to='/'><img src={logo} alt="logo"/></Link></div>
+        <div style={{'display':'flex','alignItems':'center'}}><div id='relo' onClick={()=>{open()}} style={{'border':'3px solid black','fontSize':'32px','width':'40px','height':'45px','color':"black",'marginLeft':'5px','textAlign':'center','borderRadius':'5px'}}><FontAwesomeIcon icon="fa-solid fa-bars" /></div><Link to='/'><img src={logo} alt="logo"/></Link></div>
        <div className='icons' style={{color:"black"}}>
        <Link to='/cart' style={{'textDecoration':'inherit','color':'inherit'}}><div className='cart-icon'><FontAwesomeIcon icon="fa-solid fa-cart-shopping" /><div className='small-number'>{totalUniqueItems}</div></div></Link></div>
        </div>
